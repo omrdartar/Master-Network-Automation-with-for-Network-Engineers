4b6cc66dd3191e3b22d578a7851af520cb9f23c7
omrdartar <omerdartaar@gmail.com>
2024-06-30 20:59:26 +0300

    6302024v3

---
+++ Master Network Automation with Python for Network Engineers/Bölüm3-Working with Text Files in Python/reading__files__into__list.py
@@ -0,0 +1,32 @@
+#READING FILES INTO A LIST
+
+# 1. f.read().splitlines()
+
+with open('configuration.txt') as f:
+    content = f.read().splitlines()
+    print(content)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
